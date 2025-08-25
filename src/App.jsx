1b9c5f2ca825aeaf4a19ed65a8be30d98e561caf
Vimal Kumar <vimal21082003@gmail.com>
2025-08-25 13:21:21 +0530

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter,Routes,Route } from "react-router"
+import Body from "./Body"
+import Profile from "./Profile"
+import Login from "./Login"
 
 function App() {
-
   return (
     <>
-      <h1>Hello World</h1>
-     
+     <BrowserRouter basename="/">
+   <Routes>
+    <Route path="/" element={<Body/>}>
+      <Route path="login" element={<Login/>} />
+      <Route path="profile" element={<Profile/>} />
+    </Route>
+   </Routes>
+   </BrowserRouter>
     </>
+  
   )
 }
 
