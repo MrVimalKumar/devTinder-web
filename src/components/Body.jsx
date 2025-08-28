eef13f7d3f816b49de4e3790a8273a9095a88fe5
Vimal Kumar <vimal21082003@gmail.com>
2025-08-28 15:51:41 +0530

    unAuthorized Redirected to Login

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,38 @@
-import Footer from "./Footer"
-import NavBar from "./NavBar"
-import { Outlet } from "react-router"
+import axios from "axios";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+import { Outlet, useNavigate } from "react-router";
+import { BASE_URL } from "../utlis/constants";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utlis/userSlice";
 
-const Body = () =>{
-    return(
-        <div>
-         <NavBar/>
-        <Outlet/>
-        <Footer/>
-        </div>
-       
-    )
-}
+const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const fetchUser = async() => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile", { withCredentials: true });
+      dispatch(addUser(res.data));
+    } catch (err) {
+        if(err.status === 401){
+            navigate("/login")
+        }
+      console.log(err.message);
 
-export default Body
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  },[]);
+
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Body;
